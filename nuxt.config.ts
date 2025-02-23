73f9f57f3fc16cad6724789ebf67d96c4192ab00
Tom Tobias <tntobias@gmail.com>
2025-02-23 13:15:46 -0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,66 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxthq/studio',
+    '@vueuse/nuxt',
+    'nuxt-og-image'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  colorMode: {
+    disableTransition: true
+  },
+
+  routeRules: {
+    '/api/search.json': { prerender: true },
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/',
+        '/docs'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  typescript: {
+    strict: false
+  },
+
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
